b928308fb3443c50669ca8dd32c5bd3fa8e52704
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2024-12-10 11:54:31 +0100

    ..

--- cub3d.h
+++ cub3d.h
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rguerrer <rguerrer@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/10 11:45:56 by rguerrer          #+#    #+#             */
-/*   Updated: 2024/12/10 11:45:56 by rguerrer         ###   ########.fr       */
+/*   Created: 2024/12/10 11:52:01 by rguerrer          #+#    #+#             */
+/*   Updated: 2024/12/10 11:52:01 by rguerrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#ifndef CUB3D_H
+# define CUB3D_H
 
-int main(int ac, char **av)
-{
-	if (ac != 2 || ft_strncmp(av[1] + ft_strlen(av[1]) - 4, ".cub", 4))
-	{
-		ft_putstr_fd("Error\nInvalid arguments\n", 2);
-		return (1);
-	}
-	else
-	{
-		if (parse_cub(av[1]) == -1)
-			return (1);
-		printf("Running game\n");
-		game_loop();
-		printf("Game over\n");
-	}
-	return (0);
-}
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include "libft/libft.h"
+# include "MLX42/MLX42.h"
+
