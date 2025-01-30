d55db6a4dcb8f3e486e9a17a138a238252eefa3c
Pablo Jimenez Torres <pjimenez@c3r6s3.42malaga.com>
2025-01-30 11:42:20 +0100

    init player done quit

--- src/game/gameLoop.c
+++ src/game/gameLoop.c
@@ -6,7 +6,7 @@
 /*   By: pjimenez <pjimenez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 19:29:10 by pjimenez          #+#    #+#             */
-/*   Updated: 2025/01/28 12:09:50 by pjimenez         ###   ########.fr       */
+/*   Updated: 2025/01/30 11:40:35 by pjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	game_loop(void *ml)
 
 void	init_the_player(t_cub *cub)
 {
-	cub->ply->plyr_x = cub->map->p_x * TILE_SIZE + TILE_SIZE / 2;
-	cub->ply->plyr_y = cub->map->p_y * TILE_SIZE + TILE_SIZE / 2;
+	cub->ply->plyr_x = (cub->map->p_x * TILE_SIZE) - TILE_SIZE / 2;
+	cub->ply->plyr_y = (cub->map->p_y * TILE_SIZE) - TILE_SIZE / 2;
 	cub->ply->fov_rd = (FOV * M_PI) / 180;
 	if (cub->map->init_view == 'N')
 		cub->ply->angle = 3 * M_PI / 2;
@@ -41,7 +41,7 @@ void	init_the_player(t_cub *cub)
 
 void	start_the_game(t_cub *cub)
 {
-	cub->mlx_p = mlx_init(S_W, S_H, "Cub3D", 0);
+	cub->mlx_p = mlx_init(S_W, S_H, "Cub3D", true);
 	init_the_player(cub);
 	mlx_loop_hook(cub->mlx_p, &game_loop, cub);
 	mlx_key_hook(cub->mlx_p, &mlx_key, cub);
